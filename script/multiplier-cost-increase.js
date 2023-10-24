36efe343c6618f1a6753a49c87a540914068159c
Yannick Yanga <cleps2012@gmail.com>
2023-10-24 10:01:28 +0200

    update deactive button (multiplier) and bonus cost

--- script/multiplier-cost-increase.js
+++ script/multiplier-cost-increase.js
@@ -18,6 +18,11 @@ function increaseMultiplierCost() {
     multiplierPurchaseCount++; // Increment the multiplier purchase count
 }
 
+function checkScoreAndEnableButton() {
+    // checks if score is enough to buy bonus button
+    multiplierButton.disabled = score < multiplierPrice;
+}
+
 multiplierButton.addEventListener('click', () => {
     if (score >= multiplierPrice) {
         score -= multiplierPrice; // Deduct the price from the score
@@ -27,3 +32,7 @@ multiplierButton.addEventListener('click', () => {
         scoreElement.textContent = score; // Update the score and label
     }
 });
+
+checkScoreAndEnableButton(); //initialise autoclicker button status on page load
+
+setInterval(checkScoreAndEnableButton, 100); //checks every 0.1s (quick refresh)
