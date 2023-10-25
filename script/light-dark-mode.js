a9ea84299bde96c0f0d3a3f4a72d3ed5a9a24c67
Yannick Yanga <cleps2012@gmail.com>
2023-10-25 14:30:43 +0200

    starting new layout

--- script/light-dark-mode.js
+++ script/light-dark-mode.js
@@ -3,6 +3,8 @@ const aside = document.querySelector("aside");
 const header = document.querySelector("header");
 const mode = document.getElementById("switch-mode-btn");
 const clickContainer = document.querySelector(".clicker-container");
+const menuMobile = document.querySelector(".menu");
+const bar = document.querySelectorAll(".bar");
 
 let nightMode = false;
 
@@ -13,6 +15,10 @@ const switchMode = () => {
         document.body.style.color = "#fff";
         mode.textContent = "Light Mode";
         header.style.backgroundColor = "#212020";
+        menuMobile.style.backgroundColor = "#212020";
+        bar.forEach((element) => {
+            element.style.backgroundColor = "#fbd81a";
+        });
         clickContainer.style.boxShadow = "rgba(0, 0, 0, 0.35) 0px 5px 15px;";
         clickContainer.style.backgroundColor = "#212020";
     } else {
@@ -20,6 +26,10 @@ const switchMode = () => {
         document.body.style.color = "black";
         mode.textContent = "Dark Mode";
         header.style.backgroundColor = "#dfe6e9";
+        menuMobile.style.backgroundColor = "#dfe6e9";
+        bar.forEach((element) => {
+            element.style.backgroundColor = "#9a5738";
+        });
         header.style.boxShadow = "0 4px 2px -2px rgba(0, 0, 0, 0.2)";
         clickContainer.style.backgroundColor = "#fff";
     }
